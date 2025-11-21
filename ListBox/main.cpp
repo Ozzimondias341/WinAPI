7fc21d9ce024eae5dd847ab65d51566ad32b960a
Максим <@DESKTOP-VML76U3>
2025-11-21 20:50:26 +0300

    ListBox Deleting DONE

---
+++ ListBox/main.cpp
@@ -0,0 +1,57 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include "resource.h"
+
+CONST CHAR* g_sz_VALUES[] = { "This","is", "my", "first", "List", "box" };
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM Param);
+
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+
+
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM Param)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
+		for (int i = 0; i < sizeof(g_sz_VALUES) / sizeof(g_sz_VALUES[0]); i++)
+		{
+			SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)g_sz_VALUES[i]);
+		}
+	}
+	break;
+
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		case IDC_ADD_BUTTON:
+			break;
+		case IDC_DELETE_BUTTON:
+		{
+			HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
+			INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+			SendMessage(hListBox, LB_DELETESTRING, i, 0);
+		}
+			break;
+
+		case WM_CLOSE:
+			EndDialog(hwnd, 0);
+		
+		}
+	return FALSE;
+	}
+}
