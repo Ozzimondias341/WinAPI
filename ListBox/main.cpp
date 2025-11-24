0e8431d112dfda28085636fd5171d9a86ac9373d
Максим <@DESKTOP-VML76U3>
2025-11-24 18:14:52 +0300

    Edit window - halfway DONE

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -6,6 +6,7 @@ CONST CHAR* g_sz_VALUES[] = { "This","is", "my", "first", "List", "box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM Param);
 BOOL CALLBACK DlgProcADD(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM Param);
+BOOL CALLBACK DlgProcEDIT(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM Param);
 
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -32,18 +33,34 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM Param)
 
 	case WM_COMMAND:
 	{
+		
+
 		switch (LOWORD(wParam))
 		{
+
+		case IDC_LIST1:
+		{
+			if (HIWORD(wParam) == LBN_DBLCLK)
+			{
+				INT index = (INT)SendDlgItemMessage(hwnd, IDC_LIST1, LB_GETCURSEL, 0, 0);
+
+				DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDC_DIALOG_EDIT), hwnd, (DLGPROC)DlgProcEDIT, (LPARAM) index);
+			}
+		}
+
 		case IDOK:
 			break;
+
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
 			break;
+
 		case IDC_ADD_BUTTON:
 		{
 			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, (DLGPROC)DlgProcADD, 0);
 		}
 			break;
+
 		case IDC_DELETE_BUTTON:
 		{
 			HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
@@ -51,6 +68,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM Param)
 			SendMessage(hListBox, LB_DELETESTRING, i, 0);
 		}
 		break;
+
 		}
 	}
 	break;
@@ -98,6 +116,49 @@ BOOL CALLBACK DlgProcADD(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM Param)
 		break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
+
+BOOL CALLBACK DlgProcEDIT(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM Param)
+{
+	CONST INT SIZE = 256;
+	CHAR sz_buffer[SIZE] = {};
+	HWND hList = GetDlgItem(GetParent(hwnd), IDC_EDIT1);
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT1));
+
+		
+
+		SendMessage(hList, LB_GETTEXT, Param, (LPARAM)sz_buffer);
+		SetDlgItemText(hwnd, IDC_EDIT1, sz_buffer);
+	}
+	break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_CONTROL);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(hList, LB_GETTEXT, Param, (LPARAM)sz_buffer);
+
+
+		case IDCANCEL:
+		
+			EndDialog(hwnd, 0);
+		}
+		break;
+	case WM_CLOSE:
+	{
+		EndDialog(hwnd, 0);
+	}
+	}
 	}
 	return FALSE;
 }
