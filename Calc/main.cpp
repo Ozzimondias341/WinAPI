d5bdd99ab984c837d8da2d1f838c9a02fde37d2e
Максим <@DESKTOP-VML76U3>
2025-12-03 21:30:44 +0300

    CALCULATOR - visual DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -1,5 +1,5 @@
 #include <Windows.h>
-//#include "resourse.h"
+#include "resource1.h"
 
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "Calc PV_521";
@@ -7,6 +7,26 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 
 
+CONST INT g_i_BUTTON_SIZE = 50;
+CONST INT g_i_INTERVAL = 2;
+
+CONST INT g_i_DOUBLE_BUTTON_SIZE = g_i_BUTTON_SIZE * 2 + g_i_INTERVAL;
+
+CONST INT g_i_DISPLAY_WIDTH = g_i_BUTTON_SIZE * 5 + g_i_INTERVAL * 4;
+CONST INT g_i_DISPLAY_HEIGHT = 22;
+
+CONST INT g_i_START_X = 10;
+CONST INT g_i_START_Y = 10;
+CONST INT g_i_BUTTON_START_X = g_i_START_X;
+CONST INT g_i_BUTTON_START_Y = g_i_START_Y + g_i_DISPLAY_HEIGHT + g_i_INTERVAL * 3;
+
+CONST INT g_i_WINDOW_WIDTH = g_i_DISPLAY_WIDTH + g_i_START_X * 2 + 16;
+CONST INT g_i_WINDOW_HEIGHT = g_i_DISPLAY_HEIGHT + g_i_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * 4 + 64;
+
+#define X_BUTTON_POSITION(position) g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_INTERVAL) * (position)
+#define Y_BUTTON_POSITION(position) g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * (position)
+
+CONST CHAR g_OPERATIONS[] = "+-*/";
 
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -41,9 +61,9 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		NULL, //exStyle
 		g_sz_WINDOW_CLASS, //Имя класса окна
 		g_sz_WINDOW_CLASS, //заголовок окна
-		WS_OVERLAPPEDWINDOW, //Стиль окна. Стили всегда зависят от класса окна. "WS_OVERLAPPEDWINDOW" - главное окно
+		WS_OVERLAPPEDWINDOW ^  WS_MAXIMIZEBOX  ^ WS_THICKFRAME, //Стиль окна. Стили всегда зависят от класса окна. "WS_OVERLAPPEDWINDOW" - главное окно
 		CW_USEDEFAULT, CW_USEDEFAULT, //Position
-		CW_USEDEFAULT, CW_USEDEFAULT, //Размер окна
+		g_i_WINDOW_WIDTH, g_i_WINDOW_HEIGHT, //Размер окна
 		NULL,
 		NULL, //Для главного окна это ResourseID главного меню, для дочернего (Control) - ResourseID дочернего окна(IDC_BUTTON_COPY)
 		hInstance,
@@ -83,17 +103,157 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			"0",
 			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_RIGHT ,
 			10, 10,
-			250, 22,
+			g_i_DISPLAY_WIDTH, g_i_DISPLAY_HEIGHT,
 			hwnd,
 			(HMENU)999,
 			GetModuleHandle(NULL),
 			NULL
 		);
+		CHAR sz_button[2] = {};
+		for (int i = 6; i >= 0; i -= 3)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				sz_button[0] = i + j + '1';
+				CreateWindowEx
+				(
+					NULL, "Button", sz_button,
+					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+					X_BUTTON_POSITION(j),
+					Y_BUTTON_POSITION(2 - i / 3),
+					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+					hwnd,
+					(HMENU)(IDC_BUTTON_1 + i + j),
+					GetModuleHandle(NULL),
+					NULL
+				);
+			}
+		}
+		CreateWindowEx
+		(
+			NULL, "Button", "0",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			X_BUTTON_POSITION(0),
+			Y_BUTTON_POSITION(3),
+			g_i_DOUBLE_BUTTON_SIZE, g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)(IDC_BUTTON_0),
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "Button", ".",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			X_BUTTON_POSITION(2),
+			Y_BUTTON_POSITION(3),
+			g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)(IDC_BUTTON_POINT),
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CHAR sz_operation[2] = "";
+		for (int i = 0; i < 4; i++)
+		{
+			sz_operation[0] = g_OPERATIONS[i];
+			CreateWindowEx
+			(
+				NULL, "Button", sz_operation,
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+				X_BUTTON_POSITION(3),
+				Y_BUTTON_POSITION(3 - i),
+				g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+				hwnd,
+				(HMENU)(IDC_BUTTON_PLUS + i),
+				GetModuleHandle(NULL),
+				NULL
+			);
+		}
+		
+			CreateWindowEx
+			(
+				NULL, "Button", "<-",
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+				X_BUTTON_POSITION(4),
+				Y_BUTTON_POSITION(0),
+				g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+				hwnd,
+				(HMENU)(IDC_BUTTON_BSP),
+				GetModuleHandle(NULL),
+				NULL
+			);
+			CreateWindowEx
+			(
+				NULL, "Button", "CLR",
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+				X_BUTTON_POSITION(4),
+				Y_BUTTON_POSITION(1),
+				g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+				hwnd,
+				(HMENU)(IDC_BUTTON_CLR),
+				GetModuleHandle(NULL),
+				NULL
+			);
+			CreateWindowEx
+			(
+				NULL, "Button", "=",
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+				X_BUTTON_POSITION(4),
+				Y_BUTTON_POSITION(2),
+				g_i_BUTTON_SIZE, g_i_DOUBLE_BUTTON_SIZE,
+				hwnd,
+				(HMENU)(IDC_BUTTON_EQUAL),
+				GetModuleHandle(NULL),
+				NULL
+			);
+		
 	}
+
+
 		break;
 
 	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_0:
 			break;
+		case IDC_BUTTON_1:
+			break;
+		case IDC_BUTTON_2:
+			break;
+		case IDC_BUTTON_3:
+			break;
+		case IDC_BUTTON_4:
+			break;
+		case IDC_BUTTON_5:
+			break;
+		case IDC_BUTTON_6:
+			break;
+		case IDC_BUTTON_7:
+			break;
+		case IDC_BUTTON_8:
+			break;
+		case IDC_BUTTON_9:
+			break;
+		case IDC_BUTTON_POINT:
+			break;
+		case IDC_BUTTON_PLUS:
+			break;
+		case IDC_BUTTON_MINUS:
+			break;
+		case IDC_BUTTON_ASTER:
+			break;
+		case IDC_BUTTON_SLASH:
+			break;
+		case IDC_BUTTON_BSP:
+			break;
+		case IDC_BUTTON_CLR:
+			break;
+		case IDC_BUTTON_EQUAL:
+			break;
+		}
+		break;
 
 	case WM_DESTROY:
 		PostQuitMessage(0);
