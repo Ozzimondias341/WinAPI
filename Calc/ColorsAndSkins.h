d956e4dbcb9efc2d7a40fa358bf81766731b8b8c
Максим <@DESKTOP-VML76U3>
2025-12-12 21:44:45 +0300

    Code structuring DONE

---
+++ Calc/ColorsAndSkins.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Windows.h>
+
+CONST CHAR g_sz_WINDOW_CLASS[] = "Calc PV_521";
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+VOID SetSkin(HWND hwnd, CONST CHAR skin[]);
+
+
+CONST INT g_i_WINDOW_COLOR = 0;
+CONST INT g_i_DISPLAY_COLOR = 1;
+CONST INT g_i_FONT_COLOR = 2;
+CONST COLORREF g_clr_COLORS[][3] =
+{
+	{RGB(7,127,164), RGB(0,0,100), RGB(255,0,0)},
+	{RGB(150,150,150),RGB(50,50,50), RGB(0,255,0)}
+};
+CONST CHAR* g_sz_SKIN[] = { "square_blue", "metal_mistral" };
--- Calc/main.cpp
+++ Calc/main.cpp
@@ -2,46 +2,8 @@
 #include <Windows.h>
 #include "resource1.h"
 #include <iostream>
-
-
-CONST CHAR g_sz_WINDOW_CLASS[] = "Calc PV_521";
-LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-VOID SetSkin(HWND hwnd, CONST CHAR skin[]);
-
-
-CONST INT g_i_BUTTON_SIZE = 50;
-CONST INT g_i_INTERVAL = 2;
-
-CONST INT g_i_DOUBLE_BUTTON_SIZE = g_i_BUTTON_SIZE * 2 + g_i_INTERVAL;
-
-CONST INT g_i_DISPLAY_WIDTH = g_i_BUTTON_SIZE * 5 + g_i_INTERVAL * 4;
-CONST INT g_i_DISPLAY_HEIGHT = g_i_BUTTON_SIZE;
-
-CONST INT g_i_FONT_HEIGHT = g_i_DISPLAY_HEIGHT - 2;
-CONST INT g_i_FONT_WIDTH = g_i_FONT_HEIGHT / 2;
-
-CONST INT g_i_START_X = 10;
-CONST INT g_i_START_Y = 10;
-CONST INT g_i_BUTTON_START_X = g_i_START_X;
-CONST INT g_i_BUTTON_START_Y = g_i_START_Y + g_i_DISPLAY_HEIGHT + g_i_INTERVAL * 3;
-
-CONST INT g_i_WINDOW_WIDTH = g_i_DISPLAY_WIDTH + g_i_START_X * 2 + 16;
-CONST INT g_i_WINDOW_HEIGHT = g_i_DISPLAY_HEIGHT + g_i_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * 4 + 64;
-
-#define X_BUTTON_POSITION(position) g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_INTERVAL) * (position)
-#define Y_BUTTON_POSITION(position) g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * (position)
-
-CONST CHAR g_OPERATIONS[] = "+-*/";
-
-CONST INT g_i_WINDOW_COLOR = 0;
-CONST INT g_i_DISPLAY_COLOR = 1;
-CONST INT g_i_FONT_COLOR = 2;
-CONST COLORREF g_clr_COLORS[][3] =
-{
-	{RGB(7,127,164), RGB(0,0,100), RGB(255,0,0)},
-	{RGB(150,150,150),RGB(50,50,50), RGB(0,255,0)}
-};
-CONST CHAR* g_sz_SKIN[] = { "square_blue", "metal_mistral" };
+#include "dimensions.h"
+#include "ColorsAndSkins.h"
 
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
